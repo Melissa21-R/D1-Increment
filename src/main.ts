84d438ec6237ec9b624e9f71116dd3f9b92f5651
Melissa21-R <108831978+Melissa21-R@users.noreply.github.com>
2025-10-09 04:44:00 +0000

    counter increases on click of the tea emoji button to show how many grams of caffine you are drinking. (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
+//Creates the Button
 const button = document.createElement("button");
 button.innerHTML = "🍵";
 
+//Display for the counter
+const display = document.createElement("div");
+let counter: number = 0;
+
+//Create the Base Text
+display.textContent = `${counter}mg of Caffine`;
+
+// add the clicking button event
+button.addEventListener("click", () => {
+  counter += 1;
+  display.textContent = `${counter}mg of Caffine`;
+});
+
+//Adds the button to the page
+document.body.appendChild(display);
 document.body.appendChild(button);
